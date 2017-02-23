dc73ba5d0dcea75ded85a7e1c0c6697246e8ecb1
David-wend <595075652@qq.com>
2017-02-23 21:28:04 +0800

    update

--- count.py
+++ count.py
@@ -5,8 +5,49 @@ import Inverted_index
 import tool
 
 
+def calculate_integrity(dictionary, word):
+    word_freq = dictionary.word_freq_dic[dictionary.word_index_dic[word]]
+    f_word_freq = dictionary.word_freq_dic.get(dictionary.word_index_dic.get(word[:-1], ""), 1)
+    return float(word_freq) / f_word_freq
+
+
+def calculate_stability(dictionary, word):
+    word_freq = dictionary.word_freq_dic[dictionary.word_index_dic[word]]
+    f_word_freq = dictionary.word_freq_dic.get(dictionary.word_index_dic.get(word[:-1], ""), 1)
+    l_word_freq = dictionary.word_freq_dic.get(dictionary.word_index_dic.get(word[1:], ""), 1)
+    return float(word_freq) / (f_word_freq + l_word_freq - word_freq)
+
+
+def calculate_independence_by_freq(dictionary, word):
+    set_i, dict_i = dictionary.transform_term_info(word)
+    f_word_set = set([])
+    l_word_set = set([])
+    for doc_id in set_i:
+        for location_id in dict_i[doc_id].location_ids:
+            if location_id > 0:
+                f_word_set.add(dictionary.doc_dic[doc_id].words[location_id - 1])
+            if location_id < len(dictionary.doc_dic[doc_id].words) - 1:
+                l_word_set.add(dictionary.doc_dic[doc_id].words[location_id + 1])
+    return [float(len(f_word_set)) / (dictionary.word_freq_dic[dictionary.word_index_dic[word]] + 1),
+            float(len(l_word_set)) / (dictionary.word_freq_dic[dictionary.word_index_dic[word]] + 1)]
+
+
+def calculate_independence(dictionary, word):
+    set_i, dict_i = dictionary.transform_term_info(word)
+    f_word_set = set([])
+    l_word_set = set([])
+    for doc_id in set_i:
+        for location_id in dict_i[doc_id].location_ids:
+            if location_id > 0:
+                f_word_set.add(dictionary.doc_dic[doc_id].words[location_id - 1])
+            if location_id < len(dictionary.doc_dic[doc_id].words) - 1:
+                l_word_set.add(dictionary.doc_dic[doc_id].words[location_id + 1])
+    return [1 - float(1) / (len(f_word_set) + 1), 1 - float(1) / (1 + len(l_word_set))]
+
+
 def get_co_name():
     i_dic = Inverted_index.InvertDic()
+    i_dic.init_all_dic()
     candidate_list = i_dic.word_index_dic.keys()
 
     ids_dic = {}
@@ -20,7 +61,7 @@ def get_co_name():
                     continue
                 if i_dic.add_term_bound(candidate_list[i], candidate_list[j]):
                     ids, locations = i_dic.get_co_occurrence_info(candidate_list[i], candidate_list[j])
-                    if len(ids) > 5:
+                    if len(ids) > 2:
                         i_dic.add_new_term(candidate_list[i], candidate_list[j])
                         new_word = candidate_list[i] + i_dic.index_word_dic[i_dic.word_comb_word_dic[
                             i_dic.word_index_dic[candidate_list[j]]][-1]]
@@ -32,14 +73,28 @@ def get_co_name():
         for temp in result_list:
             try:
                 lines.append(temp[0] + "@@" + str(temp[1]) + "@@" + str(
-                    len(i_dic.word_comb_word_dic[i_dic.word_index_dic[temp[0]]])) + "@@" + "##".join(
+                    round(calculate_integrity(i_dic, temp[0]), 4)) + "@@" + str(
+                    round(calculate_stability(i_dic, temp[0]), 4)) + "@@" + "##".join(
+                    [str(round(x, 4)) for x in calculate_independence(i_dic, temp[0])]) + "@@" + "##".join(
+                    [str(round(x, 4)) for x in calculate_independence_by_freq(i_dic, temp[0])]) + "@@" + "##".join(
                     [str(x) for x in ids_dic[temp[0]]]))
             except KeyError:
                 print temp[0]
                 continue
         tool.write_file("./dict/word_co.txt", lines, "a")
+        try:
+            del result_dic[u"新闻"]
+            del result_dic[u"搜狐"]
+        except:
+            pass
         candidate_list = result_dic.keys()
 
 
 if __name__ == '__main__':
-    pass
+    i_dic = Inverted_index.InvertDic()
+    i_dic.init_all_dic()
+
+    get_co_name()
+
+    calculate_independence(i_dic, u"出")
+
