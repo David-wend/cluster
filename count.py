96671b24b7e5d019fc69df8606db9c48dad16d47
David-wend <595075652@qq.com>
2017-03-02 20:40:26 +0800

    update

--- count.py
+++ count.py
@@ -10,10 +10,127 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn import tree
 from sklearn import cross_validation
 from IPython.display import Image, display
-import pydotplus
 import matplotlib.pyplot as plt
+from pandas import DataFrame
+import jieba
+import remove_duplicate
 
 __author__ = 'david'
+
+
+def get_main_class(array, num_set):
+    '''获取数组中的主类并计算类个数所占的比重
+
+    主类需要满足，该类未被映射，该类新闻在数组中占的比例最大
+
+    :param
+        array：正确聚类结果中某个类在预测聚类结果中的映射
+        num_set：已经被映射的聚类标号的集合
+    :return
+        tag：主类标号
+        pre：主类所占比重
+        right_num：主类数目
+    '''
+    calculate_array = np.array([0] * (array.max() + 1))
+    series = pd.value_counts(array)  # 获得各个元素的计数表
+    for i in series.index:
+        if i not in num_set:
+            calculate_array[i] = series[i]
+    right_num = calculate_array.max()
+    pre = float(right_num) / len(array)
+    tag = calculate_array.argmax()
+    while tag in num_set:
+        tag += 1
+    return tag, pre, right_num
+
+
+def get_pre(true_result, forecast_result):
+    '''计算聚类结果的准确性
+
+    将正确结果与聚类结果进行映射，将分错点标记为-2
+    准确率计算公式：正确聚类的个数/样本总数
+
+    :param
+        true_result: 聚类正确结果数组
+        forecast_result: 聚类预测结果数组
+    :return
+        pre：准确率
+        forecast_result: 已将错误结果标记为-2的聚类预测结果数组
+    '''
+
+    true_result = np.array(true_result, dtype=float)
+    forecast_result = np.array(forecast_result, dtype=float)
+    temp_true_result = np.array(true_result)
+    temp_forecast_result = np.array(forecast_result)
+    last_pre = 0
+    num_set = set()
+
+    correspond_array = DataFrame([], index=['true_value', 'cluser_value', 'pre', 'right_num', 'len'])
+    for i in range(len(np.unique(true_result))):
+        temp_correspond_array = DataFrame(get_cluser_tag(temp_true_result, temp_forecast_result, num_set),
+                                          columns=['true_value', 'cluser_value', 'pre', 'right_num', 'len'])
+        temp_correspond_array = temp_correspond_array.sort_values(by=['right_num'], ascending=False)
+        temp_tag_num = temp_correspond_array.index[0]
+        num_set.add(temp_correspond_array['cluser_value'][temp_tag_num])
+        num = temp_correspond_array['true_value'][temp_tag_num]
+        boolean = temp_true_result != num
+        temp_true_result = temp_true_result[boolean]
+        temp_forecast_result = temp_forecast_result[boolean]
+        correspond_array[i] = temp_correspond_array.ix[temp_tag_num]
+    correspond_array = correspond_array.T
+
+    for i in range(len(correspond_array.index)):
+        pre = correspond_array['pre'][i]
+        main_class_num = correspond_array['true_value'][i]
+        lenth = correspond_array['len'][i]
+        last_pre += pre * lenth
+        cluser_value = correspond_array['cluser_value'][i]
+        arr = np.array(boolean_calculate(true_result == main_class_num, forecast_result != cluser_value, 'and'))
+        forecast_result[arr] += true_result[arr] / 100  # 将分错的点的类标记记为-2
+        arr = np.array(boolean_calculate(true_result == main_class_num, forecast_result == cluser_value, 'and'))
+        forecast_result[arr] = i  # 将分对的点与原来正确结果的类标号对应
+    return last_pre / len(true_result), forecast_result
+
+
+def get_cluser_tag(cluser_array, forecast_result, num_set):
+    '''对正确结果与聚类结果进行映射
+
+    :param
+        cluser_array：正确聚类结果数组
+        forecast_result：预测聚类结果数组
+        num_set：已被映射聚类标号的集合
+    :return
+        correspond_array：DataFrame对象，column=['原始类号','对应类号','准确率','正确聚类个数','类总数']
+    '''
+    correspond_array = []
+    for i in np.unique(cluser_array):
+        temp_forecast_result = forecast_result[cluser_array == i]
+        main_class_num, pre, right_num = get_main_class(temp_forecast_result, num_set)
+        correspond_array.append([i, main_class_num, pre, right_num, len(temp_forecast_result)])
+    return correspond_array
+
+
+def boolean_calculate(arr1, arr2, calculate):  # 对两个数组进行布尔运算
+    '''对两个数组的每一项进行布尔运算
+
+    :param
+        arr1：第一个数组
+        arr2：第二个数组
+        calculate：运算方式，可选['not','and','or']
+    :return
+        result：布尔运算结果
+    '''
+    result = [True] * len(arr1)
+    if calculate == 'and':
+        for i in range(len(arr1)):
+            result[i] = arr1[i] and arr2[i]
+    if calculate == 'or':
+        for i in range(len(arr1)):
+            result[i] = arr1[i] or arr2[i]
+    if calculate == 'not':
+        for i in range(len(arr1)):
+            result[i] = not arr1[i]
+    return result
 
 
 def calculate_integrity(dictionary, word):
@@ -140,18 +257,17 @@ def remove_non_sense_word(words, freq, values):
     candidate_remove = {}
     for i in range(len(words)):
         if freq[i] > 2:
-            if values[i][0] > 0.6 and values[i][1] > 0.6 and values[i][2] > 0.49 \
-                    and values[i][3] > 0.49 and np.mean([values[i][2:]]) > 0.49:
+            if values[i][0] > 0.7 and values[i][1] > 0.52 and values[i][2] > 0.6:
                 # print words[i], calculate_total_weight(freq[i], values[i])
                 result[words[i]] = calculate_total_weight(freq[i], values[i])
                 if words[i][:-1] in result:
                     candidate_remove[words[i][:-1]] = result[words[i][:-1]]
                     # if result[words[i][:-1]] < 4:
-                    del result[words[i][:-1]]
+                    # del result[words[i][:-1]]
                 if words[i][1:] in result:
                     candidate_remove[words[i][1:]] = result[words[i][1:]]
                     # if result[words[i][1:]] < 4:
-                    del result[words[i][1:]]
+                    # del result[words[i][1:]]
     result_list = sorted(result.items(), key=lambda x: x[1])  # 排序比较keys
     return result_list
 
@@ -183,60 +299,49 @@ def split_data_set(data, label, num, flag0=0, flag1=0):
     return train_X, train_Y, test_X, test_Y
 
 
-if __name__ == '__main__':
-    # i_dic = Inverted_index.InvertDic()
-    # i_dic.init_all_dic()
-    # get_co_name()
-
-    # words, freq, values, doc_ids, word_index_dic = load_data()
-    # result_list = remove_non_sense_word(words, freq, values)
-    # print "==" * 10
-    # for i in result_list:
-    #     print i[0], i[1]
-
-    # result = []
-    # for i in range(len(words)):
-    #     result.append(words[i] + "," + str(freq[i]) + "," + ",".join([str(x) for x in values[i]]) + "," + str(0))
-    # tool.write_file("./dict/word_weight.txt", result ,"w")
-
+def train_clf():
     word_weights = pd.read_csv("./dict/word_weight.txt", header=None, sep=",")
     col_names = ["word_name", "freq", "integrity", "stability",
                  "independence_l", "independence_r", "label"]
     word_weights.columns = col_names
     label = word_weights["label"]
-    words = word_weights["word_name"]
+    words_pd = word_weights["word_name"]
     word_weights.drop("label", axis=1, inplace=True)
     word_weights.drop("word_name", axis=1, inplace=True)
-
-    train_x, train_y, test_x, test_y = split_data_set(word_weights, label, 100, flag0=0, flag1=0)
-    lr_model = LogisticRegression(C=1.0, penalty='l2')
-    lr_model.fit(word_weights, label)
-    pre_y = lr_model.predict(word_weights)
-    print classification_report(label, pre_y)
-    print lr_model.coef_
-    print lr_model.intercept_
-
-    rt_clf = RandomForestClassifier(n_estimators=5)
-    rt_clf.fit(word_weights, label)
-    pre_y = rt_clf.predict(word_weights)
-    print classification_report(label, pre_y)
 
     dt_clf = tree.DecisionTreeClassifier(max_depth=5)
     dt_clf.fit(word_weights, label)
     pre_y = dt_clf.predict(word_weights)
     print classification_report(label, pre_y)
-    print words[pre_y == 1]
+    return words_pd[pre_y == 1].index
 
-    # dot_data = tree.export_graphviz(dt_clf, out_file=None, feature_names=word_weights.columns, class_names="index",
-    #                                 filled=True, rounded=True, special_characters=True)
-    # graph = pydotplus.graph_from_dot_data(dot_data)
-    # img = Image(graph.create_png())
-    # display(img)
-    # graph.write_pdf("data.pdf")
 
-    scores = cross_validation.cross_val_score(lr_model, word_weights, label, cv=5)
-    print scores.mean()
-    scores = cross_validation.cross_val_score(rt_clf, word_weights, label, cv=5)
-    print scores.mean()
-    scores = cross_validation.cross_val_score(dt_clf, word_weights, label, cv=5)
-    print scores.mean()
+def calculate_sim(word_i, word_j):
+    pass
+
+
+def get_word_distribution(word):
+    pass
+
+
+if __name__ == '__main__':
+    i_dic = Inverted_index.InvertDic()
+    i_dic.init_all_dic()
+    # get_co_name()
+
+    words, freq, values, doc_ids, word_index_dic = load_data()
+    # result_list = remove_non_sense_word(words, freq, values)
+    # print "==" * 10
+    # for i in result_list:
+    #     print i[0], i[1]
+
+    word_index = train_clf()
+    # jieba.add_word("裸贷", freq = 30)
+    for w in word_index:
+        print words[w],
+        # if len(words[w])>3:
+        for i in jieba.cut(words[w]):
+            print i,
+        print
+
+    remove_duplicate.count_similar()
